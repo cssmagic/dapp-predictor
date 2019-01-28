2c27affaca0bcc997da6a9211f2997a5b63d98a3
cssmagic <cssmagic.cn@gmail.com>
2019-01-28 23:44:22 +0800

    Update files organization and code style.

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/img/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/hello-world.vue'; // @ is an alias to /src
 
 @Component({
   components: {
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/hello-world.vue';
 
-describe('HelloWorld.vue', () => {
+describe('hello-world.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
